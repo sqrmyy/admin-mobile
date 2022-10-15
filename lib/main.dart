94d51c721d062510c04f09457a519e51f291c39b
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-10-15 09:25:36 +0900

    サイドナビ

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,45 @@ class AdminMobilePage extends StatelessWidget {
   }
 }
 
+class SideNavigation extends StatefulWidget {
+  @override
+  _SideNavigationState createState() => _SideNavigationState();
+}
+
+class _SideNavigationState extends State<SideNavigation> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (index) {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      destinations: [
+        NavigationRailDestination(
+          icon: Icon(Icons.thumbs_up_down),
+          label: Text('ThumbsUpDown'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.people),
+          label: Text('People'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.face),
+          label: Text('Face'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.bookmark),
+          label: Text('Bookmark'),
+        ),
+      ],
+    );
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
